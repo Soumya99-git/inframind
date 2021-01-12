9058f8ce61399f19fab8e11ab55b8360ad6fac3f
Soumya Chatterjee <soumyachatterjee@pop-os.localdomain>
2021-01-12 11:48:54 +0530

    pickle-com

--- feedback_analysis.py
+++ feedback_analysis.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle5 as pickle
 import os
 os.chdir(r"/home/soumyachatterjee/Desktop/INFRAMIND/PROJECT/")
 model_path = "model.sav"
@@ -6,3 +6,7 @@ model = pickle.load(open(model_path,"rb"))
 
 def feedback(s):
     return(model.predict([s])[0])
+
+
+if __name__ == "__main__":
+    print(feedback("this is a good problem"))
